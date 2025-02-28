818ecb0002b81d0a3cb415675f04024bad4d323f
hmarinhoo <hellenmcordeiro@gmail.com>
2025-02-28 09:41:47 -0300

    Commit2

--- src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/money_control_api/controller/CategoryController.java
@@ -1,16 +1,50 @@
 package br.com.fiap.money_control_api.controller;
 
-import br.com.fiap.money_control_api.model.Category;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.money_control_api.model.Category;
 
 @RestController
 public class CategoryController {
-    @RequestMapping(path="/home",produces="application/json",method = {RequestMethod.GET})
-	public Category index(){
-		return new Category(1L,"Educação","book");
+
+	private List<Category> repository = new ArrayList<>();
+
+	@GetMapping("/categories") // Método get retorna todas as categorias
+	public List<Category> index() { // mocky são dados fixos para variáveis de teste
+		return repository;
 	}
-    
+
+	@PostMapping("/categories")
+	// @ResponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<Category> create(@RequestBody Category category) {
+		System.out.println("Cadastrando categoria" + category.getName());
+		repository.add(category);
+		return ResponseEntity.status(201).body(category);
+	}
+
+	@GetMapping("/categories/{id}") // Método vai retornar a categoria que eu estou pedindo
+	public ResponseEntity<Category> get(@PathVariable Long id) {
+		System.out.println("Buscando categoria" + id);
+		var category = repository.stream()
+				.filter(c -> c.getId().equals(id)).findFirst();
+		/*
+		 * stream para mandar os dados em partes pelo túnel. filter para filtrar o que
+		 * eu quero que passe por ele.
+		 * c -> c === é uma => arrow function do js,
+		 */
+
+		if (category.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(category.get());
+	}
+
 }
--- src/main/java/br/com/fiap/money_control_api/model/Category.java
+++ src/main/java/br/com/fiap/money_control_api/model/Category.java
@@ -1,29 +1,40 @@
 package br.com.fiap.money_control_api.model;
 
+import java.util.Random;
+
 public class Category {
     private Long id;
     private String name;
     private String icon;
+
     public Category(Long id, String name, String icon) {
-        this.id = id;
+        this.id = Math.abs(new Random().nextLong());
+        // math.abs é para pegar um valor absoluto. o restante é para pegar um id
+        // aleatório.
         this.name = name;
         this.icon = icon;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getIcon() {
         return icon;
     }
+
     public void setIcon(String icon) {
         this.icon = icon;
     }
